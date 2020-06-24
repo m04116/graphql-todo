b33f825e123a0096157259596dd43b01f2a4f9ec
Kostiantyn Rodin <konstantin.rodin@mediastream.ag>
2020-06-24 10:31:17 +0300

    Update repository link

--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -8,7 +8,7 @@ export const Navbar: React.FC = () => (
       <a href="/" className="brand-logo">Test GraphQL + React + Typescript</a>
       <ul className="right hide-on-med-and-down">
         <li >
-          <a href="/" className="navbar-item">
+          <a href="https://github.com/m04116/graphql-todo" className="navbar-item">
             <i className="material-icons px1">
               text_snippet
             </i>
